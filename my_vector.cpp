9c8dac7cd6c1e2e6a51b435da04d557d2ff3ef6e
vadzimko <vvadzimko@gmail.com>
2018-05-24 23:06:14 +0300

    optimization added

---
+++ my_vector.cpp
@@ -0,0 +1,5 @@
+//
+// Created by vadzimko on 18.05.18.
+//
+
+#include "my_vector.h"
---
+++ my_vector.h
@@ -0,0 +1,8 @@
+//
+// Created by vadzimko on 18.05.18.
+//
+
+#ifndef BIGINT_MY_VECTOR_H
+#define BIGINT_MY_VECTOR_H
+
+#endif //BIGINT_MY_VECTOR_H
