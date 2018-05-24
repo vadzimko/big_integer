6a470cf15ef89cac9c4ce9085c5c94f675b05464
vadzimko <vvadzimko@gmail.com>
2018-05-24 23:35:46 +0300

    copying array improved

--- my_vector.cpp
+++ my_vector.cpp
@@ -1,5 +1,134 @@
-//
-// Created by vadzimko on 18.05.18.
-//
-
 #include "my_vector.h"
+#include <cassert>
+#include <algorithm>
+#include <cstring>
+#include <iostream>
+
+my_vector::my_vector()
+        : is_small_(true), size_(0) {}
+
+my_vector::my_vector(size_t size)
+        : is_small_(size <= SMALL_SIZE), size_(size) {
+    if (!is_small_) {
+        to_big();
+    }
+}
+
+my_vector::my_vector(size_t size, uint32_t element)
+        : is_small_(size <= SMALL_SIZE), size_(size) {
+
+    if (is_small_) {
+        for (size_t i = 0; i < size; i++) {
+            data_.small[i] = element;
+        }
+    } else {
+        data_.big = std::make_shared<std::vector<uint32_t >>(size, element);
+    }
+}
+
+my_vector::my_vector(my_vector const& other) {
+    *this = other;
+}
+
+my_vector &my_vector::operator=(my_vector const &other) {
+    size_ = other.size_;
+    is_small_ = other.is_small_;
+
+    if (is_small_) {
+//        for (size_t i = 0; i < size_; i++) {
+//            data_.small[i] = other.data_.small[i];
+//        }
+        std::memcpy(data_.small, other.data_.small, size_ * sizeof(uint32_t));
+        data_.big = nullptr;
+    } else {
+        data_.big = std::make_shared<std::vector<uint32_t >>(*other.data_.big);
+    }
+
+    return *this;
+}
+
+size_t my_vector::size() const {
+    return size_;
+}
+
+size_t my_vector::capacity() const {
+    if (is_small_) {
+        return SMALL_SIZE;
+    } else {
+        return data_.big->capacity();
+    }
+}
+
+void my_vector::push_back(uint32_t const &element) {
+    if (is_small_ && size_ < SMALL_SIZE) {
+        data_.small[size_] = element;
+        ++size_;
+        return;
+    } else if (is_small_) {
+        is_small_ = false;
+        to_big();
+    }
+
+    data_.big->push_back(element);
+    size_++;
+}
+
+void my_vector::pop_back() {
+    assert(size_ > 0);
+
+    if (!is_small_) {
+        data_.big->pop_back();
+    }
+    size_--;
+}
+
+uint32_t &my_vector::operator[](size_t index) {
+    if (is_small_) {
+        return data_.small[index];
+    } else {
+        return (*data_.big)[index];
+    }
+}
+
+uint32_t const &my_vector::operator[](size_t index) const {
+    if (is_small_) {
+        return data_.small[index];
+    } else {
+        return (*data_.big)[index];
+    }
+}
+
+void my_vector::to_big() {
+    if (is_small_) {
+        return;
+    }
+
+    data_.big = std::make_shared<std::vector<uint32_t>>(size_);
+
+    for (size_t i = 0; i < SMALL_SIZE; i++) {
+        (*data_.big)[i] = data_.small[i];
+    }
+}
+
+
+void my_vector::assign(size_t size, uint32_t element) {
+    my_vector that(size, element);
+    std::swap(*this, that);
+}
+
+void my_vector::resize(size_t size, uint32_t element) {
+    size_ = size;
+    if (is_small_ && size <= SMALL_SIZE) {
+        for (size_t i = size_; i < size; i++) {
+            data_.small[i] = element;
+        }
+    } else {
+        if (is_small_) {
+            is_small_ = false;
+            to_big();
+        }
+        data_.big->resize(size, element);
+    }
+}
+
+
--- my_vector.h
+++ my_vector.h
@@ -1,8 +1,42 @@
-//
-// Created by vadzimko on 18.05.18.
-//
-
 #ifndef BIGINT_MY_VECTOR_H
 #define BIGINT_MY_VECTOR_H
+
+#include <cstdio>
+#include <cstdint>
+#include <vector>
+#include <memory>
+
+struct my_vector {
+    my_vector();
+    explicit my_vector(size_t size);
+    my_vector(size_t size, uint32_t element);
+    my_vector(my_vector const& other);
+    my_vector& operator=(my_vector const& other);
+
+    ~my_vector() = default;
+
+    size_t size() const;
+    size_t capacity() const;
+    void push_back(uint32_t const& element);
+    void pop_back();
+    void assign(size_t size, uint32_t element);
+    void resize(size_t size, uint32_t element);
+
+    uint32_t& operator[](size_t index);
+    uint32_t const& operator[](size_t index) const;
+
+private:
+    static const size_t SMALL_SIZE = 3;
+    bool is_small_;
+    size_t size_;
+    struct data {
+        std::shared_ptr<std::vector <uint32_t>> big;
+        uint32_t small[SMALL_SIZE] = {0};
+        data() : big(nullptr) {}
+        ~data() {big = nullptr;}
+    };
+    data data_;
+    void to_big();
+};
 
 #endif //BIGINT_MY_VECTOR_H
